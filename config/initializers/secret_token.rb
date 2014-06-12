afd976a0333f5525fa95337480e7b9fa767fce1b
U-ECD\ychaturvedi <ychaturvedi@CMLT-YCHATURVED.ExperianConsumerDirect.com>
2014-06-12 11:41:35 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '2766a52549a8d80cb41bd14455fabc2ef6dd5e4785d0fc5e21180ee807854e692687aaa5c596db0f494bd63995467639b7b569bd7d2f9a445715841eda5e208f'
